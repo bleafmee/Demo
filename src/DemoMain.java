ece4a3db98a9f6def58480e7f95195f33d5a81d5
Elaine Loo <bleafmee@gmail.com>
2016-12-07 17:44:25 +0800

    First commit

---
+++ src/DemoMain.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrator on 12/5/2016.
+ */
+import java.awt.*;
+import java.applet.*;
+/*
+ <applet code="sim" width=300 height=300>
+ </applet>
+ */
+
+/**
+ * My Demo Class
+ */
+public class DemoMain extends Applet
+{
+    //test
+    String msg=" hello world";
+    public void init()
+    {
+        msg+="init()--->";
+        setBackground(Color.green);
+    }
+    public void start()
+    {
+        msg+="start()--->";
+        setForeground(Color.blue);
+
+    }
+    public void paint(Graphics g)
+    {
+        msg+="paint()--->";
+        g.drawString(msg,200,50);
+    }
+}
