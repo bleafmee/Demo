9a194bef4e6ff2d0436b5cd9d88c01d4568e9896
Elaine Loo <bleafmee@gmail.com>
2016-12-10 23:41:39 +0800

    orange

--- src/DemoMain.java
+++ src/DemoMain.java
@@ -17,7 +17,7 @@ public class DemoMain extends Applet
     public void init()
     {
         msg+="init()--->";
-        setBackground(Color.red);
+        setBackground(Color.orange);
     }
     public void start()
     {
